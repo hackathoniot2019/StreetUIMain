04cf3cb45fc33433e04f5c73b249d180597d10f4
Abramov Denis <denmiracle@mail.ru>
2019-03-30 12:08:40 +0300

    1
    1

---
+++ StreetUIMain/StreetUIMain/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StreetUIMain
+//
+//  Created by Denis Abramov on 30/03/2019.
+//  Copyright © 2019 Denis Abramov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
